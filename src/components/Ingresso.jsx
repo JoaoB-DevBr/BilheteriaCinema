9ca8fc7a821dd4bc67f15bf57b8ee0dd55fbcc33
João <contatojbdev@gmail.com>
2026-08-24 08:59:51 -0300

    feat: adicionando catalogo de filmes

--- src/components/Ingresso.jsx
+++ src/components/Ingresso.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useState } from 'react'
+import "../css/style.css"
 
-const Ingresso = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+//ARRAY - catálogo
+const catalogo = [
+    { id: 1, nome: "Homem-Aranha: Um Novo Dia", preco: 30.00, disponivel: true, quantidade: 0 },
+    { id: 2, nome: "A Odisseia", preco: 30.00, disponivel: true, quantidade: 0 },
+    { id: 3, nome: "Michael", preco: 30.00, disponivel: false, quantidade: 0 },
+    { id: 4, nome: "Toy Story 5", preco: 30.00, disponivel: true, quantidade: 0 },
+
+];
 
 export default Ingresso
+
